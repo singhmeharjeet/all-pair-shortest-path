7239f6b5bfee914c771182abccb605d5dde626b5
Meharjeet Singh <meharjeet_singh@sfu.ca>
2024-04-13 02:09:12 -0700

    made 1 process work and little bit refactoring

--- src/main_mpi.cpp
+++ src/main_mpi.cpp
@@ -1,12 +1,13 @@
 #include <assert.h>
+#include <math.h>
+#include <mpi.h>
 #include <stdlib.h>
-#include <chrono>
+
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
 #include <tuple>
 #include <vector>
-#include <mpi.h>
-#include <math.h>
 
 #include "../core/csv.h"
 #include "../core/get_time.h"
@@ -18,26 +19,24 @@ const bool _print = true;
 const bool _print = false;
 #endif
 
-#define ROOT 0
+#define ROOT	0
 #define MPI_TAG 1
-#define TRUE 1
-#define FALSE 0   
-#define MIN(A, B) (A < B) ? A : B
+#define DONE	true
 
 typedef struct {
-    int rank;
-    int row, col;
-    int p, q;
-    MPI_Comm comm;
-    MPI_Comm row_comm;
-    MPI_Comm col_comm;
+	int rank;
+	int row, col;
+	int processes, chunks_per_row, chunk_size;
+	MPI_Comm comm;
+	MPI_Comm row_comm;
+	MPI_Comm col_comm;
 } GRID_INFO;
 
 const auto INF = std::numeric_limits<double>::max();
 using Edge = std::tuple<int, double, int>;
 
 class Graph {
-	std::vector<double> sol;	
+	std::vector<double> sol;
 	std::string e_file;
 	int row_size;
 
@@ -59,17 +58,19 @@ class Graph {
 	}
 
 	public:
-	Graph(const std::string& e_file, const int num_nodes) {
+	Graph(const std::string &e_file, const int num_nodes) {
 		this->e_file = e_file;
 		this->row_size = num_nodes;
 	}
 
-	int get_row_size() {
+	const int get_row_size() const {
 		return this->row_size;
 	}
-
-	std::vector<double> get_matrix() {
-		return this->sol;
+	const std::string &get_file() const {
+		return this->e_file;
+	}
+	std::vector<double> &get_matrix() {
+		return (this->sol);
 	}
 
 	void read() {
@@ -123,81 +124,51 @@ class Graph {
 };
 
 const int at(int i, int j, int n);
-void setup_grid(GRID_INFO *grid);
-int is_valid_fox(int p, int n);
-void send_sub_mtrx(std::vector<double> mtrx, int n, int q);
-double *process_mtrx(GRID_INFO *grid, double *time, double *mtrx_A, int n);
+void initialize(int argc, char **argv, GRID_INFO *grid, int num_nodes);
+void deinitialize(GRID_INFO *grid);
+bool is_valid_fox(int p, int n);
+double *process_mtrx(const GRID_INFO &grid, double *time, double *mtrx_A, int n);
 void floyd_warshall(double *A, double *B, double *C, int n);
+inline void floyd_warshall(int row_size, std::vector<double> &sol);
 void print_mtrx(double *mtrx, int row_size);
+void read_args(int argc, char **argv, std::string &e_file, int &num_nodes);
+void read_csv(const GRID_INFO &grid, Graph &graph);
+bool send_chunks(GRID_INFO &grid, Graph &graph);
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 	std::cout << std::fixed << std::setprecision(2);
 
-	cxxopts::Options options(
-		"main_serial", "Calculate All Pair Shortest Path using serial execution");
-	options.add_options(
-		"", {
-				{"file", "Input graph file path", cxxopts::value<std::string>()->default_value("./input_graphs/10Edges.csv")},
-				{"nodes", "Number of Nodes in the file", cxxopts::value<int>()->default_value("10")},
-			});
+	std::string e_file;
+	int num_nodes;
+	read_args(argc, argv, e_file, num_nodes);
 
-	auto cl_options = options.parse(argc, argv);
-	const std::string e_file = cl_options["file"].as<std::string>();
-	const int num_nodes = cl_options["nodes"].as<int>();
+	GRID_INFO grid;
+	Graph graph(e_file, num_nodes);
 
-	std::cout << "Input Graph File: " << e_file << std::endl;
-	std::cout << "Number of Nodes: " << num_nodes << std::endl;
+	initialize(argc, argv, &grid, num_nodes);
+	read_csv(grid, graph);
 
-	// set up MPI environment
-    MPI_Init(&argc, &argv);
-    GRID_INFO grid;
-    setup_grid(&grid);	
-
-	int n = num_nodes; 
-	// double *mtrx[n] = new int [n * n];
-	std::vector<double> mtrx;
-	if (grid.rank == ROOT) {
-		Graph graph(e_file, num_nodes);
-		graph.read();
-		std::cout << "Solution Matrix before Floyd Warshall" << std::endl;
-		graph.print();
-
-		if (!is_valid_fox(grid.p, n)) {
-			fprintf(stderr, "Fox algorithm can't be applied with a matrix of size %d and %d processes.\nAborting...\n", n, grid.p);
-            MPI_Abort(MPI_COMM_WORLD, 0);
-            exit(1);
-		}
-		mtrx = graph.get_matrix();
-
+	auto wasSerial = send_chunks(grid, graph);
+	if (wasSerial) {
+		return 0;
 	}
 
-	if (grid.rank == ROOT && grid.p > 1) {
-		send_sub_mtrx(mtrx, n, grid.q);
-	}
+	auto chunk_size = grid.chunk_size;
 
-	const int m = n / grid.q;
-	double *mtrx_A = new double[m*m];
-	if (grid.p > 1) {
-		MPI_Recv(mtrx_A, m * m, MPI_INT, ROOT, MPI_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	double mtrx_A[chunk_size * chunk_size];
 
-	}
-	else {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				mtrx_A[at(i,j,n)] = mtrx[at(i,j,n)];
-			}
-		}
-	}
+	MPI_Recv(
+		mtrx_A, chunk_size * chunk_size,
+		MPI_DOUBLE,
+		0,
+		MPI_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 	double time;
-	double *mtrx_C = process_mtrx(&grid, &time, mtrx_A, n);
-	if (grid.p > 1) {
-		delete[] mtrx_A;
-	}
+	double *mtrx_C = process_mtrx(grid, &time, mtrx_A, graph.get_row_size());
 
-	double *mtrx_F = new double[n * n];
-	MPI_Gather(mtrx_C, m*m, MPI_INT, mtrx_F, m*m, MPI_INT, ROOT, MPI_COMM_WORLD);
-	delete[] mtrx_C; 
+	double *mtrx_F = new double[graph.get_row_size() * graph.get_row_size()];
+	MPI_Gather(mtrx_C, chunk_size * chunk_size, MPI_INT, mtrx_F, chunk_size * chunk_size, MPI_INT, ROOT, MPI_COMM_WORLD);
+	delete[] mtrx_C;
 
 	// timer t;
 	// t.start();
@@ -206,29 +177,36 @@ int main(int argc, char* argv[]) {
 
 	if (grid.rank == ROOT) {
 		std::cout << "\nSolution Matrix after Floyd Warshall" << std::endl;
-		print_mtrx(mtrx_F, n);
+		print_mtrx(mtrx_F, graph.get_row_size());
 		fprintf(stderr, "\nExecution Time: %10.3lf milliseconds.\n\n", time * 1000);
 	}
-    delete[] mtrx_F;
-    MPI_Comm_free(&grid.comm);
-    MPI_Comm_free(&grid.row_comm);
-    MPI_Comm_free(&grid.col_comm);
-    MPI_Finalize();
-    return 0;
+
+	delete[] mtrx_F;
+
+	deinitialize(&grid);
+	return 0;
 }
 
-const int at(int i, int j, int n){
+const int at(int i, int j, int n) {
 	// for speed purposes
 	return i * n + j;
 }
+void initialize(int argc, char **argv, GRID_INFO *grid, int n) {
+	MPI_Init(&argc, &argv);
 
-void setup_grid (GRID_INFO *grid) {
-	MPI_Comm_size(MPI_COMM_WORLD, &(grid->p));
+	MPI_Comm_size(MPI_COMM_WORLD, &(grid->processes));
 	MPI_Comm_rank(MPI_COMM_WORLD, &(grid->rank));
 
-	grid->q = sqrt(grid->p);
+	if (!is_valid_fox(grid->processes, n) && grid->rank == ROOT) {
+		fprintf(stderr, "Fox algorithm can't be applied with a matrix of size %d and %d processes.\nAborting...\n", n, grid->processes);
+		MPI_Abort(MPI_COMM_WORLD, 1);
+		exit(1);
+	}
 
-	int dim[2] = {grid->q, grid->q};
+	grid->chunks_per_row = sqrt(grid->processes);
+	grid->chunk_size = n / grid->chunks_per_row;
+
+	int dim[2] = {grid->chunks_per_row, grid->chunks_per_row};
 	int periods[2] = {1, 1};
 	MPI_Cart_create(MPI_COMM_WORLD, 2, dim, periods, 1, &(grid->comm));
 
@@ -236,7 +214,7 @@ void setup_grid (GRID_INFO *grid) {
 	MPI_Comm_rank(grid->comm, &(grid->rank));
 	MPI_Cart_coords(grid->comm, grid->rank, 2, coords);
 	grid->row = coords[0];
-	grid-> col = coords[1];
+	grid->col = coords[1];
 
 	coords[0] = 0;
 	coords[1] = 1;
@@ -246,68 +224,73 @@ void setup_grid (GRID_INFO *grid) {
 	coords[1] = 0;
 	MPI_Cart_sub(grid->comm, coords, &(grid->col_comm));
 }
-
-int is_valid_fox(int p, int n) {
+void deinitialize(GRID_INFO *grid) {
+	MPI_Comm_free(&(grid->comm));
+	MPI_Comm_free(&(grid->row_comm));
+	MPI_Comm_free(&(grid->col_comm));
+	MPI_Finalize();
+}
+bool is_valid_fox(int p, int n) {
+	// p = number of processes
+	// n = row size of the matrix
+	// q = number of processors per row
 	int q = sqrt(p);
-	if (q*q == p && n % q == 0) {
-		return TRUE;
+	if (q * q == p && n % q == 0) {
+		return true;
 	}
-	return FALSE;
+	return false;
 }
 
-void send_sub_mtrx(std::vector<double> mtrx, int n, int q) {
-	int m = n / q;
-	double (*sub_mtrx) = new double[m*m];
+double *process_mtrx(const GRID_INFO &grid, double *time, double *mtrx_A, int n) {
+	const int src = (grid.row + 1) % grid.chunks_per_row;
+	const int dst = (grid.row - 1 + grid.chunks_per_row) % grid.chunks_per_row;
 
-	int dst = 0;
-	for (int k = 0; k < q; k++) {
-		for (int l = 0; l < q; l++) {
-			for (int i = 0; i < m; i++) {
-				for (int j = 0; j < m; j++) {
-					sub_mtrx[at(i, j, m)] = mtrx[at(i + (k * m), j + (l * m), m)];
-				}
-			}
-			MPI_Send(sub_mtrx, m * m, MPI_INT, dst++, MPI_TAG, MPI_COMM_WORLD);	// assign sub matrix for all processes from root to the last process
-		}
-	}
-	delete[] sub_mtrx;
-}
+	const auto size_sqr = grid.chunk_size * grid.chunk_size;
 
-double *process_mtrx(GRID_INFO *grid, double *time, double *mtrx_A, int n) {
-	int m = n / grid->q;
-	int src = (grid->row + 1) % grid->q;
-	int dst = (grid->row - 1 + grid->q) % grid->q;
+	double temp_A[size_sqr];
+	double mtrx_B[size_sqr];
+	double *mtrx_C = new double[size_sqr];
 
-	double *temp_A = new double[m * m];
-	//assert(temp_A != nullptr);
-	double *mtrx_B = new double[m * m];
-	//assert(mtrx_B != nullptr);
-	double *mtrx_C = new double[m * m];
-	//assert(mtrx_C != nullptr);
-	memcpy(mtrx_C, mtrx_A, m * m * sizeof(double));
+	// mtrx_A has the chunk of the matrix
+	memcpy(mtrx_C, mtrx_A, size_sqr * sizeof(double));
 
-	int iter;
 	*time = MPI_Wtime();
-	for (iter = 1; iter < n; iter <<= 1) {
-		memcpy(mtrx_B, mtrx_C, m * m * sizeof(int));
-		for (int stage = 0; stage < grid->q; stage++) {
-			int bcast_root = (grid->row + stage) % grid->q;
-			if (bcast_root == grid->col) {
-				MPI_Bcast(mtrx_A, m * m, MPI_INT, bcast_root, grid->row_comm);
-				floyd_warshall(mtrx_A, mtrx_B, mtrx_C, m);
+	for (auto iter = 1; iter < n; iter <<= 1) {
+		memcpy(mtrx_B, mtrx_C, size_sqr * sizeof(double));
+
+		for (int stage = 0; stage < grid.chunks_per_row; stage++) {
+			int bcast_root = (grid.row + stage) % grid.chunks_per_row;
+			if (bcast_root == grid.col) {
+				MPI_Bcast(mtrx_A, size_sqr, MPI_DOUBLE, bcast_root, grid.row_comm);
+				floyd_warshall(mtrx_A, mtrx_B, mtrx_C, grid.chunk_size);
 			} else {
-				MPI_Bcast(temp_A, m * m, MPI_INT, bcast_root, grid->row_comm);
-				floyd_warshall(temp_A, mtrx_B, mtrx_C, m);
+				MPI_Bcast(temp_A, size_sqr, MPI_DOUBLE, bcast_root, grid.row_comm);
+				floyd_warshall(temp_A, mtrx_B, mtrx_C, grid.chunk_size);
 			}
-			MPI_Sendrecv_replace(mtrx_B, m * m, MPI_INT, dst, MPI_TAG, src, MPI_TAG, grid->col_comm, MPI_STATUS_IGNORE);
+			MPI_Sendrecv_replace(mtrx_B, size_sqr, MPI_DOUBLE, dst, MPI_TAG, src, MPI_TAG, grid.col_comm, MPI_STATUS_IGNORE);
 		}
 	}
 	*time = MPI_Wtime() - *time;
-	delete[] temp_A;
-	delete[] mtrx_B;
 	return mtrx_C;
 }
 
+inline void floyd_warshall(int row_size, std::vector<double> &sol) {
+	int i, j, k;
+
+	for (k = 0; k < row_size; k++) {
+		for (i = 0; i < row_size; i++) {
+			// above picked source
+			for (j = 0; j < row_size; j++) {
+				const auto sum = sol[at(i, k, row_size)] + sol[at(k, j, row_size)];
+				auto &val = sol[at(i, j, row_size)];
+
+				if (sum < val) {
+					val = sum;
+				}
+			}
+		}
+	}
+}
 inline void floyd_warshall(double *A, double *B, double *C, int n) {
 	// double (*A)[n] = (double (*)[n]) _A;
 	// double (*B)[n] = (double (*)[n]) _B;
@@ -316,9 +299,8 @@ inline void floyd_warshall(double *A, double *B, double *C, int n) {
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			for (int k = 0; k < n; k++)
-				C[at(i,j,n)] = MIN(C[at(i,j,n)], A[at(i,k,n)] + B[at(k,j,n)]);
-}	
-
+				C[at(i, j, n)] = std::min(C[at(i, j, n)], A[at(i, k, n)] + B[at(k, j, n)]);
+}
 void print_mtrx(double *mtrx, int row_size) {
 	std::cout << "\t";
 	for (int x = 0; x < row_size; x++) {
@@ -329,7 +311,7 @@ void print_mtrx(double *mtrx, int row_size) {
 	for (int i = 0; i < row_size; i++) {
 		std::cout << i << "\t";
 		for (int j = 0; j < row_size; j++) {
-			const auto elem = mtrx[at(i,j,row_size)];
+			const auto elem = mtrx[at(i, j, row_size)];
 			if (elem > 1000000) {
 				std::cout << "INF\t";
 				continue;
@@ -338,5 +320,76 @@ void print_mtrx(double *mtrx, int row_size) {
 		}
 		std::cout << std::endl;
 	}
+}
+void read_args(int argc, char **argv, std::string &e_file, int &num_nodes) {
+	cxxopts::Options options(
+		"main_mpi", "Calculate All Pair Shortest Path using MPI execution");
+	options.add_options(
+		"", {
+				{"file", "Input graph file path", cxxopts::value<std::string>()->default_value("./input_graphs/10Edges.csv")},
+				{"nodes", "Number of Nodes in the file", cxxopts::value<int>()->default_value("10")},
+			});
 
+	auto cl_options = options.parse(argc, argv);
+	e_file = cl_options["file"].as<std::string>();
+	num_nodes = cl_options["nodes"].as<int>();
+}
+void read_csv(const GRID_INFO &grid, Graph &graph) {
+	if (grid.rank == ROOT) {
+		std::cout << "Input Graph File: " << graph.get_file() << std::endl
+				  << "Number of Nodes: " << graph.get_row_size() << std::endl
+				  << "Chunks: " << grid.processes << std::endl
+				  << "Chunk/row: " << grid.chunks_per_row << std::endl
+				  << std::endl;
+
+		std::cout << "Solution Matrix before Floyd Warshall" << std::endl;
+
+		graph.read();
+		graph.print();
+	}
+}
+bool send_chunks(GRID_INFO &grid, Graph &graph) {
+	if (grid.rank == ROOT) {
+		// Early exit if there is only one process (ROOT)
+		if (grid.processes == 1) {
+			timer t;
+			t.start();
+			floyd_warshall(graph.get_row_size(), graph.get_matrix());  // serial floyd warshall
+			auto time = t.stop();
+
+			std::cout << "\nSolution Matrix after Floyd Warshall" << std::endl;
+			graph.print();
+			std::cout << "\nTime taken: " << std::setprecision(6) << time << " seconds\n";
+
+			deinitialize(&grid);
+			return DONE;  // was serial
+		}
+
+		auto chunks_per_row = grid.chunks_per_row;
+		auto chunk_size = grid.chunk_size;
+
+		// Break the Adj Matrix into chucks(blocks) and send them to all processes
+		double sub_mtrx[chunk_size * chunk_size];
+
+		for (int chunk_row = 0; chunk_row < chunks_per_row; chunk_row++) {
+			for (int chunk_col = 0; chunk_col < chunks_per_row; chunk_col++) {
+				for (int i = 0; i < chunk_size; i++) {
+					for (int j = 0; j < chunk_size; j++) {
+						sub_mtrx[at(i, j, chunk_size)] = graph.get_matrix()[at(
+							(chunk_row * chunk_size) + i,
+							(chunk_col * chunk_size) + j,
+							chunks_per_row * chunk_size)];
+					}
+				}
+				// Sending & Receiving the chunks is checked; Dont change now
+				// It will send to root also to save the complexity of handling root separately
+				MPI_Send(
+					sub_mtrx, chunk_size * chunk_size,
+					MPI_DOUBLE,
+					(chunk_row * chunks_per_row) + chunk_col,
+					MPI_TAG, MPI_COMM_WORLD);
+			}
+		}
+	}
+	return !DONE;  // was parallel
 }
