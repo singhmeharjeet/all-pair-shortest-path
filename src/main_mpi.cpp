1c74e21cec3165e26a4a7bb2b56f31d08586f9d5
Anh Vu <vuanhkhoa22002@gmail.com>
2024-04-12 17:17:42 -0700

    mpi so far

--- src/main_mpi.cpp
+++ src/main_mpi.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <vector>
 #include <mpi.h>
+#include <math.h>
 
 #include "../core/csv.h"
 #include "../core/get_time.h"
@@ -36,9 +37,14 @@ const auto INF = std::numeric_limits<double>::max();
 using Edge = std::tuple<int, double, int>;
 
 class Graph {
-	std::vector<std::vector<double>> sol;	// 2D vector 
+	std::vector<double> sol;	
 	std::string e_file;
 	int row_size;
+
+	inline const int at(const int i, const int j) const {
+		// for speed purposes
+		return i * row_size + j;
+	}
 
 	bool validateInputs() const {
 		if (row_size <= 0) {
@@ -58,6 +64,14 @@ class Graph {
 		this->row_size = num_nodes;
 	}
 
+	int get_row_size() {
+		return this->row_size;
+	}
+
+	std::vector<double> get_matrix() {
+		return this->sol;
+	}
+
 	void read() {
 		if (!validateInputs()) {
 			return;
@@ -65,19 +79,15 @@ class Graph {
 
 		io::CSVReader<3> edges_file(e_file);
 
-		// Initialize the 2D vector
+		sol.resize(row_size * row_size, INF);
+
 		for (int i = 0; i < row_size; i++) {
-			std::vector<double> row(row_size, INF);
-			sol.push_back(row);
-		}
-		// Set all path from any vertex i to itself to 0
-		for (int i = 0; i < row_size; i++) {
-			sol[i][i] = 0;
+			sol[at(i, i)] = 0;
 		}
 
 		Edge edge;
 		while (edges_file.read_row(std::get<0>(edge), std::get<1>(edge), std::get<2>(edge))) {
-			sol[std::get<0>(edge)][std::get<2>(edge)] = std::get<1>(edge);
+			sol[at(std::get<0>(edge), std::get<2>(edge))] = std::get<1>(edge);
 		}
 	}
 
@@ -100,7 +110,7 @@ class Graph {
 		for (int i = 0; i < row_size; i++) {
 			std::cout << i << "\t";
 			for (int j = 0; j < row_size; j++) {
-				const auto elem = sol[i][j];
+				const auto elem = sol[at(i, j)];
 				if (elem > 1000000) {
 					std::cout << "INF\t";
 					continue;
@@ -110,34 +120,15 @@ class Graph {
 			std::cout << std::endl;
 		}
 	}
-
-	void floydWarshall() {
-		if (!validateInputs()) {
-			return;
-		}
-
-		int i, j, k;
-
-		for (k = 0; k < row_size; k++) {
-			// Pick all vertices as source one by one
-			for (i = 0; i < row_size; i++) {
-				// Pick all vertices as destination for the
-				// above picked source
-				for (j = 0; j < row_size; j++) {
-					// If vertex k is on the shortest path from
-					// i to j, then update the value of
-					// sol_matrix[i][j]
-					const auto sum = sol[i][k] + sol[k][j];
-					auto& val = sol[i][j];
-
-					if (sum < val) {
-						val = sum;
-					}
-				}
-			}
-		}
-	}
 };
+
+const int at(int i, int j, int n);
+void setup_grid(GRID_INFO *grid);
+int check_fox(int p, int n);
+void send_sub_mtrx(int *_mtrx, int n, int q);
+void *process_mtrx(GRID_INFO *grid, double *time, int *mtrx_A, int n);
+void floyd_warshall(int *A, int *B, int *C, int n);
+void print_mtrx(int *mtrx, int row_size);
 
 int main(int argc, char* argv[]) {
 	std::cout << std::fixed << std::setprecision(2);
@@ -157,26 +148,195 @@ int main(int argc, char* argv[]) {
 	std::cout << "Input Graph File: " << e_file << std::endl;
 	std::cout << "Number of Nodes: " << num_nodes << std::endl;
 
-	Graph graph(e_file, num_nodes);
+	// set up MPI environment
+    MPI_Init(&argc, &argv);
+    GRID_INFO grid;
+    setup_grid(&grid);	
 
-	graph.read();
-
-	if (_print) {
+	int n = num_nodes; 
+	// double *mtrx[n] = new int [n * n];
+	std::vector<double> mtrx;
+	if (grid.rank == ROOT) {
+		Graph graph(e_file, num_nodes);
+		graph.read();
 		std::cout << "Solution Matrix before Floyd Warshall" << std::endl;
 		graph.print();
+
+		if (!is_valid_fox(grid.p, n)) {
+			fprintf(stderr, "Fox algorithm can't be applied with a matrix of size %d and %d processes.\nAborting...\n", n, grid.p);
+            MPI_Abort(MPI_COMM_WORLD, 0);
+            exit(1);
+		}
+		mtrx = graph.get_matrix();
+
 	}
 
-	timer t;
-	t.start();
-	graph.floydWarshall();
-	auto end = t.stop();
+	if (grid.rank == ROOT && grid.p > 1) {
+		send_sub_mtrx(mtrx, n, grid.q);
+	}
 
-	if (_print) {
+	const int m = n / grid.q;
+	double *mtrx_A = new double[m*m];
+	if (grid.p > 1) {
+		MPI_Recv(mtrx_A, m * m, MPI_INT, ROOT, MPI_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				mtrx_A[at(i,j,n)] = mtrx[at(i,j,n)];
+			}
+		}
+	}
+
+	double time;
+	double *mtrx_C = process_mtrx(&grid, &time, mtrx_A, n);
+	if (grid.p > 1) {
+		delete[] mtrx_A;
+	}
+
+	double *mtrx_F = new double[n * n];
+	MPI_Gather(mtrx_C, m*m, MPI_INT, mtrx_F, m*m, MPI_INT, ROOT, MPI_COMM_WORLD);
+	delete[] mtrx_C; 
+
+	// timer t;
+	// t.start();
+	// graph.floydWarshall();
+	// auto end = t.stop();
+
+	if (grid.rank == ROOT) {
 		std::cout << "\nSolution Matrix after Floyd Warshall" << std::endl;
-		graph.print();
+		print_final(mtrx_F, n);
+		fprintf(stderr, "\nExecution Time: %10.3lf milliseconds.\n\n", time * 1000);
+	}
+    delete[] mtrx_F;
+    MPI_Comm_free(&grid.comm);
+    MPI_Comm_free(&grid.row_comm);
+    MPI_Comm_free(&grid.col_comm);
+    MPI_Finalize();
+    return 0;
+}
+
+const int at(int i, int j, int n){
+	// for speed purposes
+	return i * n + j;
+}
+
+void setup_grid (GRID_INFO *grid) {
+	MPI_Comm_size(MPI_COMM_WORLD, &(grid->p));
+	MPI_Comm_rank(MPI_COMM_WORLD, &(grid->rank));
+
+	grid->q = sqrt(grid->p);
+
+	int dim[2] = {grid->q, grid->q};
+	int periods[2] = {1, 1};
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dim, periods, 1, &(grid->comm));
+
+	int coords[2];
+	MPI_Comm_rank(grid->comm, &(grid->rank));
+	MPI_Cart_coords(grid->comm, grid->rank, 2, coords);
+	grid->row = coords[0];
+	grid-> col = coords[1];
+
+	coords[0] = 0;
+	coords[1] = 1;
+	MPI_Cart_sub(grid->comm, coords, &(grid->row_comm));
+
+	coords[0] = 1;
+	coords[1] = 0;
+	MPI_Cart_sub(grid->comm, coords, &(grid->col_comm));
+}
+
+int is_valid_fox(int p, int n) {
+	int q = sqrt(p);
+	if (q*q == p && n % q == 0) {
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void send_sub_mtrx(std::vector<double> mtrx, int n, int q) {
+	int m = n / q;
+	double (*sub_mtrx) = new double[m*m];
+
+	int dst = 0;
+	for (int k = 0; k < q; k++) {
+		for (int l = 0; l < q; l++) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					sub_mtrx[at(i, j, m)] = mtrx[at(i + (k * m), j + (l * m), m)];
+				}
+			}
+			MPI_Send(sub_mtrx, m * m, MPI_INT, dst++, MPI_TAG, MPI_COMM_WORLD);	// assign sub matrix for all processes from root to the last process
+		}
+	}
+	delete[] sub_mtrx;
+}
+
+void *process_mtrx(GRID_INFO *grid, double *time, double *mtrx_A, int n) {
+	int m = n / grid->q;
+	int src = (grid->row + 1) % grid->q;
+	int dst = (grid->row - 1 + grid->q) % grid->q;
+
+	double *temp_A = new double[m * m];
+	//assert(temp_A != nullptr);
+	double *mtrx_B = new double[m * m];
+	//assert(mtrx_B != nullptr);
+	double *mtrx_C = new double[m * m];
+	//assert(mtrx_C != nullptr);
+	memcpy(mtrx_C, mtrx_A, m * m * sizeof(double));
+
+	int iter;
+	*time = MPI_Wtime();
+	for (iter = 1; iter < n; iter <<= 1) {
+		memcpy(mtrx_B, mtrx_C, m * m * sizeof(int));
+		for (int stage = 0; stage < grid->q; stage++) {
+			int bcast_root = (grid->row + stage) % grid->q;
+			if (bcast_root == grid->col) {
+				MPI_Bcast(mtrx_A, m * m, MPI_INT, bcast_root, grid->row_comm);
+				floyd_warshall(mtrx_A, mtrx_B, mtrx_C, m);
+			} else {
+				MPI_Bcast(temp_A, m * m, MPI_INT, bcast_root, grid->row_comm);
+				floyd_warshall(temp_A, mtrx_B, mtrx_C, m);
+			}
+			MPI_Sendrecv_replace(mtrx_B, m * m, MPI_INT, dst, MPI_TAG, src, MPI_TAG, grid->col_comm, MPI_STATUS_IGNORE);
+		}
+	}
+	*time = MPI_Wtime() - *time;
+	delete[] temp_A;
+	delete[] mtrx_B;
+	return mtrx_C;
+}
+
+inline void floyd_warshall(double *A, double *B, double *C, int n) {
+	// double (*A)[n] = (double (*)[n]) _A;
+	// double (*B)[n] = (double (*)[n]) _B;
+	// double (*C)[n] = (double (*)[n]) _C;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int k = 0; k < n; k++)
+				C[at(i,j,n)] = MIN(C[at(i,j,n)], A[at(i,k,n)] + B[at(k,j,n)]);
+}	
+
+void print_mtrx(double *mtrx, int row_size) {
+	std::cout << "\t";
+	for (int x = 0; x < row_size; x++) {
+		std::cout << x << "\t";
+	}
+	std::cout << std::endl;
+
+	for (int i = 0; i < row_size; i++) {
+		std::cout << i << "\t";
+		for (int j = 0; j < row_size; j++) {
+			const auto elem = mtrx_F[at(i,j,row_size)];
+			if (elem > 1000000) {
+				std::cout << "INF\t";
+				continue;
+			}
+			std::cout << elem << "\t";
+		}
+		std::cout << std::endl;
 	}
 
-	std::cout << "\nTime taken: " << std::setprecision(6) << end << " seconds\n";
-
-	return 0;
 }
